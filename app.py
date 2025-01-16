ca696d28279c5c6e3a3586ba6c448605ebf7b36f
Lord Xen <x.lord.xen@gmail.com>
2025-01-16 20:44:43 +0500

    first commit zazu

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    request
+)
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def get_index():
+    pass
+    return render_template('index.html')
+
+@app.route('/log', methods=['GET', 'POST'])
+def get_log():
+    pass
+    return render_template('log.html')
+
+@app.route('/reg', methods=['GET', 'POST'])
+def get_reg():
+    pass
+    return render_template('reg.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=8080
+    )
